fc304fd14edb7f2530f97c6caf15f52df9bb03e7
Atharva Patwardhan <thecaffeinesadhu@gmail.com>
2020-07-13 19:37:56 +0530

    Added update and delete functionality to backend

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -16,4 +16,26 @@ router.route('/add').post((req, res) => {
 	newExercise.save().then(() => res.json('User Added!')).catch(err => res.status(400).json('Error: '+ err));
 });
 
+router.route('/:id').get((req, res) => {
+	Exercise.findById(req.params.id)
+	.then(exercises => res.json(exercises)).catch(err => res.status(400).json('Error: '+ err));
+});
+
+router.route('/:id').delete((req, res) => {
+	Exercise.findByIdAndDelete(req.params.id)
+	.then(exercises => res.json('Exercise Deleted!')).catch(err => res.status(400).json('Error: '+ err));
+});
+
+router.route('/update/:id').post((req, res) => {
+	Exercise.findById(req.params.id)
+	.then(exercises => {
+		exercises.username = req.body.username;
+		exercises.description = req.body.description;
+		exercises.duration = Number(req.body.duration);
+		exercises.date = Date.parse(req.body.date);
+
+		exercises.save().then(() => res.json('Exercise Updated!')).catch(err => res.status(400).json('Error: '+ err));
+	});
+});
+
 module.exports = router;
